83f855d97abca657439241543b8d13ac25375d58
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-01-26 09:01:05 -0500

    Some comments about the Hypothesis/testtools fix.

--- src/txkube/testing/_testcase.py
+++ src/txkube/testing/_testcase.py
@@ -26,10 +26,19 @@ class TestCase(TesttoolsTestCase):
     # recognize.
     def setup_example(self):
         try:
+            # TesttoolsTestCase starts without this attribute set at all.  Get
+            # us back to that state.  It won't be set at all on the first
+            # setup_example call, nor if the previous run didn't have a failed
+            # expectation.
             del self.force_failure
         except AttributeError:
             pass
 
     def teardown_example(self, ignored):
         if getattr(self, "force_failure", False):
+            # If testtools things it's time to stop, translate this into a
+            # test failure exception that Hypothesis can see.  This lets
+            # Hypothesis know when it has found a falsifying example.  Without
+            # it, Hypothesis can't see which of its example runs caused
+            # problems.
             self.fail("expectation failed")
