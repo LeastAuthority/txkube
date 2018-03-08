4779262e256efae3cc362d4c7d6429eb10ae4580
Craig Rodrigues <rodrigc@FreeBSD.org>
2018-03-08 09:33:16 -0800

    Add whitespace

--- src/txkube/test/test_memory.py
+++ src/txkube/test/test_memory.py
@@ -13,6 +13,7 @@ from testtools.matchers import Equals, Is, IsInstance, AfterPreprocessing
 
 from twisted.python.compat import unicode
 
+
 from ..testing.integration import kubernetes_client_tests
 from ..testing.strategies import iobjects
 from ..testing import TestCase
